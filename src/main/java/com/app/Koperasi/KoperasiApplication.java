6ff09015b49e4ba5ba455da110c354a5594d325f
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-06-18 10:09:29 +0700

    feat: initialize boilerplate

---
+++ src/main/java/com/app/Koperasi/KoperasiApplication.java
@@ -0,0 +1,13 @@
+package com.app.Koperasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoperasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoperasiApplication.class, args);
+	}
+
+}
