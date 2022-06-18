ea878c6b95b4a1349418af359ab135fbd553f700
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-06-18 10:29:23 +0700

    feat: initialize post member usecase

--- src/main/java/com/app/Koperasi/controller/MemberController.java
+++ src/main/java/com/app/Koperasi/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.app.Koperasi.controller;
 
 import com.app.Koperasi.domain.Member;
+import com.app.Koperasi.usecase.MemberUsecase;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @PostMapping
     public Member addMember(@RequestBody Member member) {
-        return member;
+        return MemberUsecase.addMember(member);
     }
 }
---
+++ src/main/java/com/app/Koperasi/usecase/MemberUsecase.java
@@ -0,0 +1,9 @@
+package com.app.Koperasi.usecase;
+
+import com.app.Koperasi.domain.Member;
+
+public class MemberUsecase {
+    public static Member addMember(Member member) {
+        return member;
+    }
+}
