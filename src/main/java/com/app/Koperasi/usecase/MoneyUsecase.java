8899147087a1b37272bd4c4bf4cbba2b5a5e6047
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-06-19 15:02:44 +0700

    feat: initialize save money usecase

---
+++ src/main/java/com/app/Koperasi/usecase/MoneyUsecase.java
@@ -0,0 +1,10 @@
+package com.app.Koperasi.usecase;
+
+import com.app.Koperasi.request.SaveMoneyRequest;
+
+public class MoneyUsecase {
+
+    public void SaveMoney(SaveMoneyRequest req) {
+
+    }
+}
